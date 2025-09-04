ed8e6350c349397251880702af2bd3b1475afc22
semusings <bhuwan.upadhyay49@gmail.com>
2025-09-03 21:27:00 -0500

    chore: update package configuration and rename project files

---
+++ Lesson 1 - Project Loom: Virtual Threads, Structured Concurrency and Scoped Values/example-code/src/main/java/dev/semusings/lab/package-info.java
@@ -0,0 +1,1 @@
+package dev.semusings.lab;
--- Lesson 1 - Project Loom: Virtual Threads, Structured Concurrency and Scoped Values/java-jep-502-stable-values/src/main/java/dev/semusings/lab/StableValueExample.java
+++
@@ -1,8 +0,0 @@
-package dev.semusings.lab;
-
-/**
- * docs: <a href="https://cr.openjdk.org/~pminborg/stable-values2/api/java.base/java/lang/StableValue.html">...</a>
- */
-public class StableValueExample {
-
-}
